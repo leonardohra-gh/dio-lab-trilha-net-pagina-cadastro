e0c7aaecf709074a53f83189f2e311466057e791
Leonardo Henrique da Rocha Araujo <leonardohra.gh@gmail.com>
2024-11-13 14:26:05 -0300

    1ª versão dos botões (com variante)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
+import Button from "./components/Button";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <h1>Home</h1>
+      <Button title={"Botão teste normal"} />
+      <Button title={"Botão teste variant 2"} variant="secondary" />
+    </>
   );
 }
 
